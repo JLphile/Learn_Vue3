b56b4fd8432681d0438b3d73d1040cff743c11ba
JLphile <lkyz001@outlook.com>
2023-06-01 22:42:36 +0800

    08_Vue的阶段案例

---
+++ 08_Vue的阶段案例/data/data.js
@@ -0,0 +1,7 @@
+const books = [
+  { id: 1, name: '《 算法导论 》', date: '2006-09', price: 85, count: 3 },
+  { id: 2, name: '《 UNIX编程艺术 》', date: '2006-02', price: 59, count: 2 },
+  { id: 3, name: '《 编程珠玑 》', date: '2008-10', price: 39, count: 3 },
+  { id: 4, name: '《 代码大全 》', date: '2006-03', price: 128, count: 2 },
+  { id: 5, name: '《 你不知道的JavaScript 》', date: '2014-08', price: 88, count: 1 },
+]
